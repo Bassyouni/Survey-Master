dad8fe2b927181360517ec621ad2e28fa43a4bce
youssef sanad <yousef_sanad@hotmail.com>
2017-12-16 04:34:42 +0200

    addSurvey front end is done, waitnig on the backend

--- web/js/dynamic-form.js
+++ web/js/dynamic-form.js
@@ -10,11 +10,13 @@
     var checkQuestionCounter = 0;
     
     var radioAnswerCounter = new Array();
+    var checkAnswerCounter = new Array();
 $(document).ready(function() {
     
     
     $('#add-text-button').click(addTextQuestion);
     $('#add-radio-button').click(addRadioQuestion);
+    $('#add-check-button').click(addCheckQuestion);
     
     
     function addTextQuestion(){
@@ -41,7 +43,7 @@ $(document).ready(function() {
                         <input name='radio-question-` + radioQuestionCounter + `' type="text" class="form-control" placeholder="Add Radio Question" id="radio-question-` + radioQuestionCounter + `" required data-validation-required-message="Please enter your Question." >
                       </div>
                     </div>
-                    <div class="check-answer-container-` + radioQuestionCounter + `">
+                    <div class="radio-answer-container-` + radioQuestionCounter + `">
                         
                     </div>
         <br>
@@ -52,6 +54,34 @@ $(document).ready(function() {
         
         $('#script-div').remove();
         alert('deleted');
+        $('body').append(`<div id="script-div">
+            <script src="js/sub-dynamic-form.js" type="text/javascript"></script>
+        </div>`);
+                
+    }
+    function addCheckQuestion(){
+        
+        checkQuestionCounter++;
+        questionCounter++;
+        checkAnswerCounter.push(0);
+        alert(checkAnswerCounter);
+        $('.question-container').append(
+                    `<div class="control-group">
+                      <div class="form-group floating-label-form-group controls">
+                        <label>Question-`+ questionCounter + `</label>
+                        <input name='check-question-` + checkQuestionCounter + `' type="text" class="form-control" placeholder="Add Check Question" id="check-question-` + checkQuestionCounter + `" required data-validation-required-message="Please enter your Question." >
+                      </div>
+                    </div>
+                    <div class="check-answer-container-` + checkQuestionCounter + `">
+                        
+                    </div>
+        <br>
+                    <button type='button' class='check-answer-button btn btn-warning' id='check-answer-button-` + checkQuestionCounter +`'>Add Answer</button>
+                    
+           <br> `
+                );
+        
+        $('#script-div').remove();
         $('body').append(`<div id="script-div">
             <script src="js/sub-dynamic-form.js" type="text/javascript"></script>
         </div>`);
--- web/js/sub-dynamic-form.js
+++ web/js/sub-dynamic-form.js
@@ -5,18 +5,32 @@
  */
 
 
-/* global radioAnswerCounter */
+/* global radioAnswerCounter, checkAnswerCounter */
 
 $(document).ready(function() {
+    /*name is 'radio-question-1-answer-2'*/
     $('.radio-answer-button').unbind().click(function (){
         var id = $(this).attr('id').replace('radio-answer-button-', '');
         radioAnswerCounter[parseInt(id-1)]++;
-        $('.check-answer-container-' + id).append(`<div class="control-group">
+        $('.radio-answer-container-' + id).append(`<div class="control-group">
                       <div class="form-group floating-label-form-group controls">
                         <label>Answer-`+ radioAnswerCounter[parseInt(id-1)] + `</label>
-                        <input name='radio-question-` + id + 'answer-' + radioAnswerCounter[parseInt(id-1)] + `' type="text" class="form-control" placeholder="Add Radio Answer here..." id="radio-question-` + id + `answer-` + radioAnswerCounter[parseInt(id-1)] + `" required data-validation-required-message="Please enter your Question." >
+                        <input name='radio-question-` + id + '-answer-' + radioAnswerCounter[parseInt(id-1)] + `' type="text" class="form-control" placeholder="Add Radio Answer here..." id="radio-question-` + id + `-answer-` + radioAnswerCounter[parseInt(id-1)] + `" required data-validation-required-message="Please enter your Question." >
                       </div>
                     </div>`);
         alert('radio-question-' + id + 'answer-' + radioAnswerCounter[parseInt(id-1)]);
+    });
+    
+    /*name is 'check-question-1-answer-2'*/
+    $('.check-answer-button').unbind().click(function (){
+        var id = $(this).attr('id').replace('check-answer-button-', '');
+        checkAnswerCounter[parseInt(id-1)]++;
+        $('.check-answer-container-' + id).append(`<div class="control-group">
+                      <div class="form-group floating-label-form-group controls">
+                        <label>Answer-`+ checkAnswerCounter[parseInt(id-1)] + `</label>
+                        <input name='check-question-` + id + '-answer-' + checkAnswerCounter[parseInt(id-1)] + `' type="text" class="form-control" placeholder="Add Radio Answer here..." id="radio-question-` + id + `-answer-` + checkAnswerCounter[parseInt(id-1)] + `" required data-validation-required-message="Please enter your Question." >
+                      </div>
+                    </div>`);
+        alert('check-question-' + id + '-answer-' + checkAnswerCounter[parseInt(id-1)]);
     });
 });
